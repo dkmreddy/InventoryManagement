279e4111c9b04733a1632968d1d4e66cc3e1fb39
Matiur Rahman Minar <minar09.bd@gmail.com>
2014-09-10 03:12:59 +0600

    garments base page refined!

--- InventoryManagement/views.py
+++ InventoryManagement/views.py
@@ -7,4 +7,4 @@ from django.http.response import HttpResponse
 from django.shortcuts import render_to_response
 
 def home(request):
-    return  render_to_response('main/signin.html')
+    return  render_to_response('base/signin.html')

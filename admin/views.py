4ab40fb9041b739836bb07ec736efcc88ea34768
Matiur Rahman Minar <minar09.bd@gmail.com>
2014-09-13 04:13:27 +0600

    All sections base templates finalized!

--- admin/views.py
+++ admin/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
 from django.http.response import HttpResponse
+from django.shortcuts import render_to_response
+
 def home(request):
-    return HttpResponse("AdminPage")
+    return render_to_response('admin/BaseTemplateForAdmin.html',{})

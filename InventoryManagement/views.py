a6b5430ee313aa9cc02207c17f23de948558295d
Raju Ahmed <shetu2153@gmail.com>
2014-09-13 12:34:28 -0700

    Log file Added with the function name.

--- InventoryManagement/views.py
+++ InventoryManagement/views.py
@@ -5,9 +5,17 @@ Created on Sep 6, 2014
 '''
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+import inspect
+from Logging.LogModule import PassMessasge,getfileInfo,error_flag,info_flag,warning_flag
+import os
+
+file_path=os.path.realpath(os.path.abspath(__file__))
 
 def home(request):
+    sModuleInfo=getfileInfo(file_path)+" : "+inspect.stack()[0][3]
+    PassMessasge(sModuleInfo, "In the home page",info_flag)
     return  render_to_response('base/signin.html',{},context_instance=RequestContext(request))
 
 def LoginApprove(request):
+    print inspect.stack()[0][3]
     print request
---
+++ Logging/LogModule.py
@@ -0,0 +1,40 @@
+from InventoryManagement.settings import PROJECT_ROOT
+import logging
+import os
+
+handlerpath=os.path.realpath(os.path.join(PROJECT_ROOT,"Logging/LogFile.log"))
+logger = logging.getLogger(__name__)
+hdlr = logging.FileHandler(handlerpath)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
+info_flag=1
+warning_flag=2
+error_flag=3
+
+def LogInfo(sModuleInfo,sDetails,iLogLevel=1,sStatus=""):
+    """
+    1- info
+    2 - warning
+    3 - error
+    """
+    if iLogLevel == 1:
+        logger.info(sModuleInfo + ' - ' + sDetails + '' + sStatus)
+    elif iLogLevel == 2:
+        logger.warning(sModuleInfo + ' - ' + sDetails + '' + sStatus)
+    elif iLogLevel == 3:
+        logger.error(sModuleInfo + ' - ' + sDetails + '' + sStatus)
+    else:
+        print "Unknown Log Level"
+    
+
+def PassMessasge(sModuleInfo,msg,level,debug=True):
+    if debug:
+        print msg
+        LogInfo(sModuleInfo, msg, level)
+    return msg
+
+def getfileInfo(path):
+    path=path.split("\\")
+    return path[-2]+"/"+path[-1]

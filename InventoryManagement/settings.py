7481fbf81bc69d6022838aa36c04ddbcffab4e69
Raju Ahmed <shetu2153@gmail.com>
2014-09-14 00:38:27 -0700

    The login is fully done for the admin

--- InventoryManagement/settings.py
+++ InventoryManagement/settings.py
@@ -17,11 +17,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'InventoryManagement',                      # Or path to database file if using sqlite3.
+        'NAME': 'TestDB',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'Admin',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }
--- InventoryManagement/views.py
+++ InventoryManagement/views.py
@@ -8,14 +8,31 @@ from django.template.context import RequestContext
 import inspect
 from Logging.LogModule import PassMessasge,getfileInfo,error_flag,info_flag,warning_flag
 import os
+from django.contrib.auth import authenticate,login
+from django.http.response import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 file_path=os.path.realpath(os.path.abspath(__file__))
 
 def home(request):
     sModuleInfo=getfileInfo(file_path)+inspect.stack()[0][3]
-    PassMessasge(sModuleInfo, "In the home page",info_flag)
+    PassMessasge(sModuleInfo, "In the Login page",info_flag)
     return  render_to_response('base/signin.html',{},context_instance=RequestContext(request))
 
 def LoginApprove(request):
     sModuleInfo=getfileInfo(file_path)+ inspect.stack()[0][3]
-    print request
+    try:
+        if request.method=="POST":
+            user_name=request.POST['username']
+            password=request.POST['password']
+            user=authenticate(username=user_name,password=password)
+            if user is not None:
+                login(request,user)
+                print user
+                return HttpResponseRedirect(reverse('admin.views.home'))
+            else:
+                PassMessasge(sModuleInfo, "(%s,%s)No Such User exists"%(user_name,password),error_flag)
+                return HttpResponseRedirect(reverse('InventoryManagement.views.home'))
+    except Exception,e:
+        PassMessasge(sModuleInfo, str(e),error_flag)
+        return HttpResponseRedirect(reverse('InventoryManagement.views.home'))
